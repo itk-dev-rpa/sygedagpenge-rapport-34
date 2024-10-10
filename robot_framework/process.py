478aa9bc43a2d6406b6feef9260e9351ece3226c
Mathias G <ghbm@aarhus.dk>
2024-10-10 13:40:58 +0200

    Added prints

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,7 +1,6 @@
 """This module contains the main process of the robot."""
 
 import os
-import uuid
 from datetime import datetime, timedelta
 from tkinter import filedialog
 
@@ -22,19 +21,23 @@ def process() -> None:
 
     year, week_number, _ = (datetime.today() - timedelta(weeks=1)).isocalendar()
 
+    print(f"Henter rapport for uge {week_number}, {year}")
     report_path = ksd_process.create_report(browser, year, week_number, year, week_number)
     cases = ksd_process.read_csv_file(report_path)
     os.remove(report_path)
 
     # Get company type on each case
+    print("Henter virksomhedstyper")
     for c in cases:
         c.company_type = cvr_lookup.cvr_lookup(c.cvr_number, os.environ['cvr_username'], os.environ['cvr_password']).company_type
 
     # Get info from ksd
     for c in cases:
+        print(f"Henter info på sag {c.case_number}")
         ksd_process.get_case_info(browser, c)
 
     excel_process.write_excel(cases, save_path)
+    os.startfile(save_path)
 
 
 if __name__ == '__main__':
