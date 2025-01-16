049ad7f2d6884075b8309f2e2687b36044a81146
Mathias G <ghbm@aarhus.dk>
2025-01-16 09:56:22 +0100

    Added retry loop

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -26,6 +26,8 @@ def process() -> None:
     cases = ksd_process.read_csv_file(report_path)
     os.remove(report_path)
 
+    print(f"Antal sager: {len(cases)}")
+
     # Get company type on each case
     print("Henter virksomhedstyper")
     for c in cases:
@@ -42,4 +44,12 @@ def process() -> None:
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
-    process()
+    for i in range(3):
+        try:
+            process()
+            break
+        except Exception:
+            if i != 2:
+                print("Hov noget gik galt... Prøver igen.")
+            else:
+                print("Processen fejlede for mange gange... Kontakt udvikler")
