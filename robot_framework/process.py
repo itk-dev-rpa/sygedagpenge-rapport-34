d5a999de386440cefe34e8152accb79844724908
Kristian Bakspace <kriba@aarhus.dk>
2025-11-07 10:28:22 +0100

    Update process.py

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -56,5 +56,5 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
 if __name__ == '__main__':
     conn_string = os.getenv("OpenOrchestratorConnString")
     crypto_key = os.getenv("OpenOrchestratorKey")
-    oc = OrchestratorConnection("Rapport 34 test", conn_string, crypto_key, "")
+    oc = OrchestratorConnection("Rapport 34 test", conn_string, crypto_key, "", "")
     process(oc)
